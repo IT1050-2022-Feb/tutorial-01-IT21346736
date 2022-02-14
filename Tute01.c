57daaa258aba05ae48f23e162c3db92ba809c16d
IT21346736 <99631353+IT21346736@users.noreply.github.com>
2022-02-14 11:15:28 +0000

    I included the calculations.

--- Tute01.c
+++ Tute01.c
@@ -8,17 +8,16 @@ int main() {
    
    int m1,m2,total;
    float average=0;
-  
-   
+
    printf("Enter the mark 1 :");
    scanf("%d",&m1);
-   
+
    printf("Enter the mark 2 :");
    scanf("%d",&m2);
-   
+
    total = m1 + m2;
    average = total / 2;
-   
+
    printf("Average is %.2f",average);
    
   return 0;
--- Tute03.c
+++ Tute03.c
@@ -13,7 +13,23 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
+
+  int n,i;
+  int sum = 0;
+
+  printf("Enter a number :");
+  scanf("%d",&n);
+   
+  i = 1;
   
+ while(i <= n)
+ {
+   sum += i;
+   i++;
+ }
+  
+
+  printf("Sum is %d",sum);
   return 0;
 }
 
